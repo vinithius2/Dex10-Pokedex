463b4ba18808065b7756afe291b127311838e17a
Marcos <marcos.filho@ebankit.com>
2024-10-31 01:27:05 +0000

    Add initial theme

--- app/src/main/java/com/vinithius/poke10/ui/MainActivity.kt
+++ app/src/main/java/com/vinithius/poke10/ui/MainActivity.kt
@@ -3,37 +3,82 @@ package com.vinithius.poke10.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.vinithius.poke10.ui.screens.PokemonDetailScreen
 import com.vinithius.poke10.ui.screens.PokemonListScreen
+import com.vinithius.poke10.ui.theme.ThemePoke10
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainNavigation()
+            ThemePoke10 {
+                MainScreen()
+            }
         }
     }
 }
 
 @Composable
-fun MainNavigation() {
+fun MainScreen() {
     val navController = rememberNavController()
-    NavHost(
-        navController = navController,
-        startDestination = "pokemonList"
-    ) {
-        composable("pokemonList") { PokemonListScreen(navController) }
-        composable("pokemonDetail") { PokemonDetailScreen(navController) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Pokémon App", color = Color.White)
+                },
+                backgroundColor = MaterialTheme.colors.primary,
+                actions = {
+                    IconButton(onClick = { /* Ação de favoritos */ }) {
+                        Icon(
+                            imageVector = Icons.Default.Favorite,
+                            contentDescription = "Favorito",
+                            tint = Color.White
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "pokemonList",
+            Modifier.padding(innerPadding)
+        ) {
+            composable("pokemonList") { PokemonListScreen(navController) }
+            composable("pokemonDetail") { PokemonDetailScreen(navController) }
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun MainNavigationPreview() {
-    MainNavigation()
+fun MainScreenPreview() {
+    ThemePoke10 {
+        MainScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenDarkPreview() {
+    ThemePoke10(darkTheme = true) {
+        MainScreen()
+    }
 }
