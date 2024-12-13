2440a70e371479b00189c372ababaabd86b32499
Marcos <marcos.filho@ebankit.com>
2024-12-13 15:53:37 +0000

    Added experimental tab page

--- app/src/main/java/com/vinithius/poke10/ui/screens/PokemonDetailScreen.kt
+++ app/src/main/java/com/vinithius/poke10/ui/screens/PokemonDetailScreen.kt
@@ -19,9 +19,11 @@ import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -30,23 +32,31 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
@@ -57,6 +67,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -88,6 +99,8 @@ import ir.ehsannarmani.compose_charts.models.BarProperties
 import ir.ehsannarmani.compose_charts.models.Bars
 import ir.ehsannarmani.compose_charts.models.LabelHelperProperties
 import ir.ehsannarmani.compose_charts.models.LabelProperties
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.getViewModel
 
 @Composable
@@ -320,20 +333,79 @@ fun SharedTransitionScope.PokemonDetailScreen(
         PokemonChart(viewModel, pokemonDetail, pokemonColor)
         PokemonIsABaby()
         PokemonEvolution(pokemonDetail)
-        PokemonDamage(pokemonDetail, viewModel)
+        // Tabs
+        TabWithPagerExample(pokemonDetail, viewModel)
+        //PokemonDamage(pokemonDetail, viewModel)
         /*
         setInfo(pokemon)
-        setIsBaby(pokemon)
         setEncounters(pokemon)
         setEggGroups(pokemon)
-        setDamage(pokemon)
         setTextEntries(pokemon)
-        setStats(pokemon)
-        setTypes(pokemon)
         setAbilities(pokemon)
-        setEvolutions(pokemon)
         setIconsMythicalAndLegendary(pokemon)
         */
+    }
+}
+
+@Composable
+fun TabWithPagerExample(pokemonDetail : Pokemon?, viewModel: PokemonViewModel = getViewModel()) {
+    val pagerState = rememberPagerState(pageCount = { 6 })
+    val tabTitles = listOf("Damage", "Encounters", "Eggs", "Abilities", "Info", "Entries")
+    val coroutineScope = rememberCoroutineScope()
+
+    LazyRow(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) {
+        items(tabTitles.size) { index ->
+            val isSelected = pagerState.currentPage == index
+            val backgroundColor =
+                if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary
+            val textColor =
+                if (isSelected) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSecondary
+
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(16.dp)) // Borda arredondada
+                    .background(backgroundColor)
+                    .clickable {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    }
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+            ) {
+                Text(
+                    text = tabTitles[index],
+                    color = textColor,
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+
+    HorizontalPager(
+        state = pagerState,
+        modifier = Modifier.fillMaxSize()
+    ) { page ->
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            when (page) {
+                0 -> PokemonDamage(pokemonDetail, viewModel)
+                1 -> Text("Página 2", modifier = Modifier.fillMaxSize())
+                2 -> Text("Página 3", modifier = Modifier.fillMaxSize())
+                3 -> Text("Página 4", modifier = Modifier.fillMaxSize())
+                4 -> Text("Página 5", modifier = Modifier.fillMaxSize())
+                else -> Text("Página desconhecida", modifier = Modifier.fillMaxSize())
+            }
+        }
     }
 }
 
