f41fd11cae1f079b4d5f75b9544e7fde6a97a4d8
Marcos <marcos.filho@ebankit.com>
2025-01-09 23:13:10 +0000

    Adjust admod

--- app/src/main/java/com/vinithius/poke10/components/AdMobComponents.kt
+++ app/src/main/java/com/vinithius/poke10/components/AdMobComponents.kt
@@ -3,7 +3,6 @@ package com.vinithius.poke10.components
 import android.util.Log
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
@@ -11,14 +10,17 @@ import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.vinithius.poke10.BuildConfig
 import com.vinithius.poke10.ui.viewmodel.PokemonViewModel
 import org.koin.androidx.compose.getViewModel
 
+// Teste Ad Unit ID
 private val adUnitIdTeste = "ca-app-pub-3940256099942544/6300978111"
 
 @Composable
 fun AdmobBanner(viewModel: PokemonViewModel = getViewModel()) {
-    val adUnitId by viewModel.adUnitId.observeAsState()
+    val adUnitId =
+        if (BuildConfig.DEBUG) adUnitIdTeste else viewModel.adUnitId.observeAsState().value
     adUnitId?.takeIf { it.isNotEmpty() }?.let { validAdUnitId ->
         AndroidView(
             modifier = Modifier.fillMaxWidth(),
